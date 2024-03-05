0380dea0875eeb3b748e031d33f83854b761b965
adithya112 <63769259+adithya112@users.noreply.github.com>
2024-03-05 09:03:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum Index/maximum-index.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim()); // size of array
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
+		    }
+		    
+		    Solution ob = new Solution();
+		    
+		    System.out.println(ob.maxIndexDiff(arr, n)); // print the result
+		}
+	}
+}
+// } Driver Code Ends
+
+
+
+
+class Solution{
+    
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    static int maxIndexDiff(int A[], int N) { 
+        
+        // Your code here
+        int minArr[] = new int[N];
+        int maxArr[] = new int[N];
+        
+        minArr[0] = A[0];
+        for(int i=1;i<N;i++){
+            minArr[i] = Math.min(A[i], minArr[i-1]);
+        }
+        
+        maxArr[N-1] = A[N-1];
+        for(int i=N-2;i>=0;i--){
+            maxArr[i] = Math.max(A[i], maxArr[i+1]);
+        }
+        
+        int i=0, j=0;
+        int maxInd=0;
+        while(i < N && j < N){
+            if(minArr[i] <= maxArr[j]){
+                maxInd = Math.max(maxInd, (j-i));
+                j += 1;
+            }else{
+                i+=1;
+            }
+        }
+        
+        return maxInd;
+    }
+}
+
+
